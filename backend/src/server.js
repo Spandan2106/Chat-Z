b53cfcece7650137d5b64e509a976c6223042bf7
Spandan2106 <sd672483@gmail.com>
2026-02-10 15:41:26 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,8 +18,8 @@ app.use("/uploads", express.static("uploads"));
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:3000", process.env.FRONTEND_URL], // Specific origins are required when credentials are true
-    methods: ["GET", "POST"],
+    origin: ["http://localhost:5173", "http://localhost:3000", process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   },
   transports: ["websocket", "polling"],
